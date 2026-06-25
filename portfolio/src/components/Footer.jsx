bb957ad728e2dbce989ac84904c0deefb1b2b00b
Aayusha Kandel <aayushakandel46@gmai.com>
2026-06-25 14:39:09 +0545

    Update links

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -6,8 +6,8 @@ function Footer() {
       <div className="flex justify-center gap-6">
         <a href="https://github.com/Aayusha497">GitHub</a>
         <a href="https://www.linkedin.com/in/aayusha-kandel-432190347/">LinkedIn</a>
-        <a href="https://www.instagram.com/aayusha.5/">Instagram</a>
-        <a href="https://www.facebook.com/aayusha.kandel.359">Facebook</a>
+        {/* <a href="https://www.instagram.com/aayusha.5/">Instagram</a>
+        <a href="https://www.facebook.com/aayusha.kandel.359">Facebook</a> */}
       </div>
 
       <p className="mt-4">+977-9703712593</p>
